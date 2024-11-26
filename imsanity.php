2065291592c1b631cf82bdbc4278b19931d210a2
Shane Bishop <shanebishop@ewww.io>
2024-11-26 10:31:18 -0700

    releasing version 2.8.6

--- imsanity.php
+++ imsanity.php
@@ -14,7 +14,7 @@ Plugin URI: https://wordpress.org/plugins/imsanity/
 Description: Imsanity stops insanely huge image uploads
 Author: Exactly WWW
 Domain Path: /languages
-Version: 2.8.5
+Version: 2.8.6
 Requires at least: 6.4
 Requires PHP: 7.4
 Author URI: https://ewww.io/about/
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'IMSANITY_VERSION', '2.8.5' );
+define( 'IMSANITY_VERSION', '2.8.6' );
 define( 'IMSANITY_SCHEMA_VERSION', '1.1' );
 
 define( 'IMSANITY_DEFAULT_MAX_WIDTH', 1920 );
